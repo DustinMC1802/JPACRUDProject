82eeba26bf3861cbc7f1e8639041d59ba5864fc2
DustinMC1802 <dustinmcollier@gmail.com>
2022-04-15 18:30:48 -0700

    Started Boot MVC project, added JPA project dependency

---
+++ FencingClubs/src/main/java/com/skilldistillery/jpafencingclubs/FencingClubsApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpafencingclubs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FencingClubsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FencingClubsApplication.class, args);
+	}
+
+}
---
+++ FencingClubs/src/test/java/com/skilldistillery/jpafencingclubs/FencingClubsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpafencingclubs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FencingClubsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
